d99058e03145e5206b2d7886d2eccb650f2cdd7f
qiaofei <qiaofei9@foxmail.com>
2023-05-28 10:05:43 +0800

    test

--- src/store/modules/user/helper.ts
+++ src/store/modules/user/helper.ts
@@ -15,9 +15,9 @@ export interface UserState {
 export function defaultSetting(): UserState {
   return {
     userInfo: {
-      avatar: 'https://raw.githubusercontent.com/Chanzhaoyu/chatgpt-web/main/src/assets/avatar.jpg',
-      name: 'ChenZhaoYu',
-      description: 'Star on <a href="https://github.com/Chanzhaoyu/chatgpt-bot" class="text-blue-500" target="_blank" >GitHub</a>',
+      avatar: 'https://tse4-mm.cn.bing.net/th/id/OIP-C.eOs-vTjZzB4DVfB-ntgxOwHaHa?pid=ImgDet&rs=1',
+      name: 'Gpt user',
+      description: '',
     },
   }
 }
